22401c2a6c7e897d122771c03a2ba3dfc7c15fc5
Oluwatobi Olowookere <tobilobaolowo350@gmail.com>
2025-08-28 12:23:00 -0400

    feat: Add failed login mechanism

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 # main.py
+import logging
 from vault.cli import main_cli
 from getpass import getpass
 from vault import storage
 
 def main():
-    master_password = getpass("Enter the master password to unlock the vault:  ")
-    
-    try:
-        #Load and decrypt vault from disk to memory
-        vault = storage.load_vault(master_password)
-    except ValueError:
-        print("X Wrong password or corrupted vault")
-        return
+    #Configure logging
+    logging.basicConfig(filename="failed_attempts.log", level=logging.WARNING, format="%(asctime)s - %(message)s")
+
+    MAX_ATTEMPTS = 3
+    attempts = 0
+
+    while attempts < MAX_ATTEMPTS:
+        master_password = getpass("Enter the master password to unlock the vault:  ")
+        try:
+            #Load and decrypt vault from disk to memory
+            vault = storage.load_vault(master_password)
+            break #successful login
+        except ValueError:
+            print("X Wrong password or corrupted vault")
+            attempts += 1
+            logging.warning(f"Failed login attempt #{attempts}")
+            print(f"Wrong password ({attempts}/{MAX_ATTEMPTS})")
+        
+    if attempts >= MAX_ATTEMPTS:
+        print("Account locked due to multiple failed login attempts.")
+        exit(1)
 
     #Pass the in-memory vault and master password to the CLI
     main_cli(vault, master_password)
